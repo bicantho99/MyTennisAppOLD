3f4b43cce26a9f09acde0463a9f20fe08c0b00b9
ntaliequach <natalie.quach03@gmail.com>
2024-12-05 00:49:08 -0800

    made everything more visible

--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,47 @@
-import { View, Text, TextInput, Button } from "react-native";
+import { View, Text, TextInput, Button, StyleSheet} from "react-native";
 import React from "react";
 
 export default function subLesson() {
   return (
-    <View>
-      <Text className="text-center mt-5">How many students here today?</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>How many students here today?</Text>
       <TextInput
         placeholder="add coaches here"
-        className="p-8 rounded-sm border-pink-300 border-2"
+        style={styles.input}
       />
-      <Text className="text-center mt-5">How many courts available today?</Text>
+      <Text style={styles.text}>How many courts available today?</Text>
       <TextInput
         placeholder="add courts here"
-        className="p-8 rounded-sm border-pink-300 border-2 mb-5"
+        style={styles.input}
       />
       <Button
         title="Learn More"
+        color="#6200ee"
         accessibilityLabel="Learn more about this purple button"
       />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#000000',
+    padding: 20,
+  },
+  text: {
+    color: '#ffffff',
+    marginBottom: 20,
+  },
+  input: {
+    color: 'white',
+    width: '90%',
+    padding: 8,
+    marginBottom: 20,
+    borderColor: 'pink',
+    borderWidth: 2,
+    borderRadius: 5,
+  }
+});
