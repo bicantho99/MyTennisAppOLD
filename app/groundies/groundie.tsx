b7d5fe4cb2a4d85227b6a99f8bf3aef81770bd2a
ntaliequach <natalie.quach03@gmail.com>
2024-12-17 02:19:25 -0800

    Style checkboxes for consistent green border and white fill, Fix checkbox visibility issues on iOS

--- app/groundies/groundie.tsx
+++ app/groundies/groundie.tsx
@@ -1,6 +1,7 @@
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import Checkbox from "expo-checkbox";
+import { transparent } from "react-native-paper/lib/typescript/styles/themes/v2/colors";
 
 export default function Groundie() {
   const tasks = [
@@ -10,17 +11,42 @@ export default function Groundie() {
     "Hit with consistent topspin",
     "Hit with improved footwork",
   ];
+
+  //React state to manage check boxes
+  const [checkedItems, setCheckedItems] = useState (
+    Array(tasks.length).fill(false)
+  );
+
+  //function for toggling box
+  const toggleBox = (index: number) => {
+    const updateCheckedItems = [...checkedItems];
+    updateCheckedItems[index] = !updateCheckedItems[index];
+    setCheckedItems(updateCheckedItems);
+  };
+
   return (
     <SafeAreaView className="bg-bgColor flex-1 flex-col">
       <View className="mx-10 gap-10">
         <Text className="text-center text-textColor text-3xl">Groundies</Text>
-
-        {tasks.map((task, index) => (
-          <View key={index} className="flex-row gap-5">
-            <Checkbox />
-            <Text className="text-textColor">{task}</Text>
-          </View>
-        ))}
+          <View className="bg-boxColor p-5 gap-5 rounded-lg shadow-sm">
+            {tasks.map((task, index) => (
+              <View key={index} className="flex-row gap-5">
+                <Checkbox
+                  value = {checkedItems[index]}
+                  onValueChange={() => toggleBox(index)}
+                  color={checkedItems[index] ? "#34eb83" : "white"}
+                  style={{
+                    borderWidth: 1,
+                    borderColor: "#34eb83",
+                    borderRadius : 3,
+                    backgroundColor: checkedItems[index] ? "#34eb83" : "white",
+                  }}
+                
+                />
+                <Text className="text-textColor">{task}</Text>
+              </View>
+            ))}
+        </View>
       </View>
     </SafeAreaView>
   );
