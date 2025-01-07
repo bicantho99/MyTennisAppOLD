cbafbff2fa3aa748c84634496eda69e3c8983604
Khanh <ngiakhanh18@gmail.com>
2025-01-06 19:14:48 -0600

    SMOOTHAF

--- app/(root)/index.tsx
+++ app/(root)/index.tsx
@@ -4,6 +4,9 @@ import {
   TouchableOpacity,
   SafeAreaView,
   ScrollView,
+  FlatList,
+  Dimensions,
+  Animated,
 } from "react-native";
 import React, { useState } from "react";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
@@ -11,14 +14,57 @@ import * as Progress from "react-native-progress";
 import Checkbox from "expo-checkbox";
 import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
 import { router } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 export default function Home() {
-  const [completedTasks, setCompletedTasks] = useState(0);
-  const totalTasks = 7;
-  const progress = completedTasks / totalTasks;
+  const { width, height } = Dimensions.get("screen");
 
+  type Challenge = {
+    day: number;
+    text: string;
+    completed: boolean;
+  };
+
+  const challenges: Challenge[] = [
+    { day: 1, text: "Approach the net 10 times", completed: false },
+    { day: 2, text: "Hit 5 aces", completed: false },
+    { day: 3, text: "Play a full match", completed: false },
+    { day: 4, text: "Win 3 games in a row", completed: false },
+    { day: 5, text: "Practice footwork for 20 mins", completed: false },
+    { day: 6, text: "Serve consistently for 10 minutes", completed: false },
+    { day: 7, text: "Play a full set", completed: false },
+  ];
+  const totalTasks = 7;
+  const [completedTasks, setCompletedTasks] = React.useState<number[]>([]);
+  const progress = completedTasks.length / challenges.length;
+  const scrollY = React.useRef(new Animated.Value(0)).current;
+  const scrollViewRef = React.useRef<FlatList>(null);
+  const ITEM_SIZE = 90;
+
+  const handleCheckboxChange = (index: number) => {
+    const isChecked = completedTasks.includes(index);
+
+    if (isChecked) {
+      // Uncheck the box, no scroll needed
+      setCompletedTasks(completedTasks.filter((task) => task !== index));
+    } else {
+      // Check the box and scroll to the next task
+      setCompletedTasks((prev) => [...prev, index]);
+      setTimeout(() => {
+        const nextIndex = index + 1;
+        if (nextIndex < challenges.length && scrollViewRef.current) {
+          // Ensure to scroll to the next item if it's within bounds
+          scrollViewRef.current.scrollToIndex({
+            index: nextIndex,
+            animated: true,
+          });
+        }
+      }, 130);
+    }
+  };
   return (
     <SafeAreaView style={{ flex: 1 }} className="bg-bgColor">
+      <StatusBar style="light" />
       <View className="mx-6 my-6">
         <View className="Header mb-5 flex-row justify-between">
           <Text className="text-textColor font-semibold text-[25px]">
@@ -31,32 +77,87 @@ export default function Home() {
           </TouchableOpacity>
         </View>
 
-        <View className="box-view box-border bg-gray-800 pl-5  pr-3 pt-5  rounded-xl gap-[5px]   border-[0.4px] ">
+        <View className="box-view  bg-slate-800 pl-5  pr-3 pt-5  border-blue-800 rounded-xl gap-[5px]   border-[0.4px]  h-[190px]">
           <View className="text-view gap-2">
-            <Text className="text-blue-300 font-bold text-[20px] mb-5">
+            <Text className="text-blue-300 font-bold text-[21px] mb-4">
               Weekly's Challenge
             </Text>
-            <Progress.Bar
-              progress={progress}
-              width={320}
-              animated={true}
-              useNativeDriver={true}
-              animationConfig={{ bounciness: 0 }}
-              animationType={"timing"}
-            />
-          </View>
-
-          <TouchableOpacity>
-            <View className="bg-blue-400 rounded-md gap-3 p-3 my-5">
-              <View className="flex-row justify-between">
-                <Text>Day 1</Text>
-                <Checkbox color={"black"} />
-              </View>
-              <Text className="text-[15px] font-semibold">
-                Approach the net 10 times
-              </Text>
+            <View className="mb-1">
+              <Progress.Bar
+                progress={progress}
+                width={320}
+                animated={true}
+                useNativeDriver={true}
+                animationConfig={{ bounciness: 0 }}
+                animationType={"timing"}
+              />
             </View>
-          </TouchableOpacity>
+            <View className="h-[90px] flex justify-between ">
+              <Animated.FlatList
+                ref={scrollViewRef} // Attach the FlatList reference
+                onScroll={Animated.event([
+                  { nativeEvent: { contentOffset: { y: scrollY } } },
+                ])}
+                data={challenges}
+                keyExtractor={(item) => String(item.day)}
+                pagingEnabled
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={{ gap: 2 }}
+                scrollEventThrottle={10} // Lower values will make scroll events fire more frequently
+                decelerationRate={0.2}
+                renderItem={({ item, index }) => {
+                  const inputRange = [
+                    -1,
+                    0,
+                    ITEM_SIZE * index,
+                    ITEM_SIZE * (index + 2),
+                  ];
+                  const opacityRange = [
+                    -1,
+                    0,
+                    ITEM_SIZE * index,
+                    ITEM_SIZE * (index + 0.5),
+                  ];
+
+                  const scale = scrollY.interpolate({
+                    inputRange,
+                    outputRange: [1, 1, 1, 0],
+                  });
+                  const opacity = scrollY.interpolate({
+                    inputRange: opacityRange,
+                    outputRange: [1, 1, 1, 0],
+                  });
+                  const isChecked = completedTasks.includes(index);
+
+                  return (
+                    <Animated.View
+                      className="w-[320px]"
+                      style={{ transform: [{ scale }], opacity }}
+                    >
+                      <View
+                        className="bg-blue-400 rounded-xl gap-3 p-[15px] mt-[8.3px] "
+                        style={{}}
+                      >
+                        <View className="flex-row justify-between ">
+                          <Text className="font-semibold opacity-[0.6px]">
+                            Day {item.day}
+                          </Text>
+                          <Checkbox
+                            value={isChecked}
+                            onValueChange={() => handleCheckboxChange(index)}
+                            color={isChecked ? "#2563eb" : "#334155"}
+                          />
+                        </View>
+                        <Text className="text-[16px] font-semibold">
+                          Approach the net 10 times
+                        </Text>
+                      </View>
+                    </Animated.View>
+                  );
+                }}
+              />
+            </View>
+          </View>
         </View>
 
         <View className="Header mt-5 flex-row justify-between mb-5">
--- app/(root)/training.tsx
+++ app/(root)/training.tsx
@@ -5,12 +5,12 @@ import {
   TextInput,
   ScrollView,
   TouchableOpacity,
+  StatusBar,
 } from "react-native";
 import React from "react";
 import { useProgramData } from "@/assets/constants/programs";
 import AntDesign from "@expo/vector-icons/AntDesign";
 import { router } from "expo-router";
-import { StatusBar } from "expo-status-bar";
 
 export default function index() {
   const { programData, setProgramData } = useProgramData();
@@ -27,73 +27,77 @@ export default function index() {
     other: ["Forehand short", "Forehand short", "Forehand short"],
   };
   return (
-    <SafeAreaView className="bg-bgColor flex-1">
-      <StatusBar style="light" />
-      <ScrollView>
-        <View className="mx-7 gap-5 pb-10">
-          <View className="flex-row justify-between items-center">
-            <Text className="text-textColor text-4xl font-bold mt-5">
-              Trainings
-            </Text>
+    // <SafeAreaView className="bg-bgColor flex-1">
+
+    <ScrollView
+      className=" bg-bgColor flex-1"
+      style={{ paddingTop: StatusBar.currentHeight || 42 }}
+    >
+      <StatusBar hidden />
+      <View className="mx-7 gap-5 pb-10">
+        <View className="flex-row justify-between items-center">
+          <Text className="text-textColor text-4xl font-bold mt-5">
+            Trainings
+          </Text>
+          <TouchableOpacity
+            onPress={() => {
+              router.push("/(edit)/addprogram");
+              programData.push(newTraining);
+              console.log(programData.length);
+            }}
+          >
+            <AntDesign
+              name="pluscircle"
+              size={22}
+              color="white"
+              className="mt-2"
+            />
+          </TouchableOpacity>
+        </View>
+        <TextInput
+          className="bg-gray-800 rounded-md h-[35px]  pl-3 "
+          placeholder="Search by name"
+          placeholderTextColor={"gray"}
+        />
+
+        {programData.map((item, index) => {
+          // const borderColorClass =
+          //   index % 2 === 0 ? "border-blue-200" : "border-blue-600";
+
+          return (
             <TouchableOpacity
-              onPress={() => {
-                router.push("/(edit)/addprogram");
-                programData.push(newTraining);
-                console.log(programData.length);
-              }}
+              key={item.title}
+              onPress={() => router.push("/(edit)/training-page")}
             >
-              <AntDesign
-                name="pluscircle"
-                size={22}
-                color="white"
-                className="mt-2"
-              />
-            </TouchableOpacity>
-          </View>
-          <TextInput
-            className="bg-gray-800 rounded-md h-[35px]  pl-3 "
-            placeholder="Search by name"
-            placeholderTextColor={"gray"}
-          />
-
-          {programData.map((item, index) => {
-            const borderColorClass =
-              index % 2 === 0 ? "border-blue-200" : "border-blue-600";
-
-            return (
-              <TouchableOpacity
-                key={item.title}
-                onPress={() => router.push("/(edit)/training-page")}
-              >
-               <View key={index}>
-                  <View
-                    className={`box-view box-border bg-gray-800 pl-5 pr-3 py-5 rounded-xl gap-[5px]  ${borderColorClass} border-[0.4px] `}
-                  >
-                    <View className="text-view gap-2">
-                      <Text className="text-blue-300 font-bold text-[17px] ">
-                        {item.title}
-                      </Text>
-                      <View className="flex-row">
-                        {item.focuses?.map((focusItem, idx) => (
-                          <Text
-                            key={idx}
-                            className="mr-3 mt-[3.5px] px-2 py-1 bg-blue-200 rounded-md text-[12px] font-medium "
-                          >
-                            {focusItem.focus}
-                          </Text>
-                        ))}
-                      </View>
-                      <Text className="text-gray-400 font-semibold">
-                        {item.description}
-                      </Text>
+              <View key={index}>
+                <View
+                  className={`box-view box-border bg-slate-800 pl-5 pr-3 py-5 rounded-xl gap-[5px] border-blue-600 border-[0.4px] `}
+                >
+                  <View className="text-view gap-2">
+                    <Text className="text-blue-300 font-bold text-[17px] ">
+                      {item.title}
+                    </Text>
+                    <View className="flex-row">
+                      {item.focuses?.map((focusItem, idx) => (
+                        <Text
+                          key={idx}
+                          className="mr-3 mt-[3.5px] px-2 py-1 bg-blue-200 rounded-md text-[12px] font-medium "
+                        >
+                          {focusItem.focus}
+                        </Text>
+                      ))}
                     </View>
+                    <Text className="text-gray-400 font-semibold">
+                      {item.description}
+                    </Text>
                   </View>
                 </View>
-              </TouchableOpacity>
-            );
-          })}
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+              </View>
+            </TouchableOpacity>
+          );
+        })}
+      </View>
+    </ScrollView>
+    // </SafeAreaView>
   );
 }
