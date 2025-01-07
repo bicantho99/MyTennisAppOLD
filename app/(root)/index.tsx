587c892610dbd80f59749b1bda907cb07ed8468c
Khanh <ngiakhanh18@gmail.com>
2025-01-06 19:26:15 -0600

    smoothAF

--- app/(root)/index.tsx
+++ app/(root)/index.tsx
@@ -3,16 +3,22 @@ import {
   Text,
   TouchableOpacity,
   SafeAreaView,
-  Dimensions,
   ScrollView,
+  FlatList,
+  Dimensions,
+  Animated,
 } from "react-native";
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import * as Progress from "react-native-progress";
 import Checkbox from "expo-checkbox";
 import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
 import { router } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 export default function Home() {
+  const { width, height } = Dimensions.get("screen");
+
   type Challenge = {
     day: number;
     text: string;
@@ -28,218 +34,221 @@ export default function Home() {
     { day: 6, text: "Serve consistently for 10 minutes", completed: false },
     { day: 7, text: "Play a full set", completed: false },
   ];
+  const totalTasks = 7;
+  const [completedTasks, setCompletedTasks] = React.useState<number[]>([]);
+  const progress = completedTasks.length / challenges.length;
+  const scrollY = React.useRef(new Animated.Value(0)).current;
+  const scrollViewRef = React.useRef<FlatList>(null);
+  const ITEM_SIZE = 90;
 
-  const scrollViewRef = useRef<ScrollView>(null);
-  const [currentTask, setCurrentTask] = useState(0);
-  const [completedTasks, setCompletedTasks] = useState<number[]>([]);
-  const totalTasks = challenges.length;
-  const progress = completedTasks.length / totalTasks;
+  const handleCheckboxChange = (index: number) => {
+    const isChecked = completedTasks.includes(index);
 
-  const screenWidth = Dimensions.get("window").width;
-  const cardWidth = screenWidth * 0.8;
-
+    if (isChecked) {
+      // Uncheck the box, no scroll needed
+      setCompletedTasks(completedTasks.filter((task) => task !== index));
+    } else {
+      // Check the box and scroll to the next task
+      setCompletedTasks((prev) => [...prev, index]);
+      setTimeout(() => {
+        const nextIndex = index + 1;
+        if (nextIndex < challenges.length && scrollViewRef.current) {
+          // Ensure to scroll to the next item if it's within bounds
+          scrollViewRef.current.scrollToIndex({
+            index: nextIndex,
+            animated: true,
+          });
+        }
+      }, 130);
+    }
+  };
   return (
     <SafeAreaView style={{ flex: 1 }} className="bg-bgColor">
-      <ScrollView>
-        <View className="mx-6 my-6">
-          <View className="Header mb-5 flex-row justify-between">
-            <Text className="text-textColor font-semibold text-[25px]">
-              Welcome
+      <StatusBar style="light" />
+      <View className="mx-6 my-6">
+        <View className="Header mb-5 flex-row justify-between">
+          <Text className="text-textColor font-semibold text-[25px]">
+            Welcome
+          </Text>
+          <TouchableOpacity>
+            <Text className="text-green-400 font-[500] text-[15px] p-2">
+              Log In
             </Text>
-            <TouchableOpacity>
-              <Text className="text-green-400 font-[500] text-[15px] p-2">
-                Log In
-              </Text>
-            </TouchableOpacity>
-          </View>
+          </TouchableOpacity>
+        </View>
 
-          <View className="box-view box-border bg-gray-800 pl-4 pr-3 pt-5 rounded-xl gap-[5px] border-[0.4px] h-[210px]">
-            <View className="text-view gap-2">
-              <Text className="text-blue-300 font-bold text-[20px] mb-2 ml-2">
-                Weekly's Challenge
-              </Text>
-              <View className="mb-2 mt-1 ml-2">
-                <Progress.Bar
-                  progress={progress}
-                  width={300}
-                  animated={true}
-                  useNativeDriver={true}
-                  animationConfig={{ bounciness: 10 }}
-                  animationType={"timing"}
-                />
-              </View>
+        <View className="box-view  bg-slate-800 pl-5  pr-3 pt-5  border-blue-800 rounded-xl gap-[5px]   border-[0.4px]  h-[190px]">
+          <View className="text-view gap-2">
+            <Text className="text-blue-300 font-bold text-[21px] mb-4">
+              Weekly's Challenge
+            </Text>
+            <View className="mb-1">
+              <Progress.Bar
+                progress={progress}
+                width={320}
+                animated={true}
+                useNativeDriver={true}
+                animationConfig={{ bounciness: 0 }}
+                animationType={"timing"}
+              />
             </View>
-
-            <View className="">
-              <ScrollView
-                ref={scrollViewRef}
-                horizontal
+            <View className="h-[90px] flex justify-between ">
+              <Animated.FlatList
+                ref={scrollViewRef} // Attach the FlatList reference
+                onScroll={Animated.event([
+                  { nativeEvent: { contentOffset: { y: scrollY } } },
+                ])}
+                data={challenges}
+                keyExtractor={(item) => String(item.day)}
                 pagingEnabled
-                showsHorizontalScrollIndicator={false}
-                onScroll={(event) => {
-                  const pageIndex = Math.round(
-                    event.nativeEvent.contentOffset.x / cardWidth
-                  );
-                  setCurrentTask(pageIndex);
-                }}
-                scrollEventThrottle={50}
-              >
-                {challenges.map((challenge, index) => (
-                  <View
-                    key={index}
-                    style={{
-                      width: cardWidth,
-                      paddingHorizontal: 0,
-                      marginRight: 0,
-                    }}
-                  >
-                    <View
-                      className="bg-blue-400 rounded-md gap-3 p-3 my-3"
-                      style={{ marginHorizontal: 8 }}
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={{ gap: 2 }}
+                scrollEventThrottle={10} // Lower values will make scroll events fire more frequently
+                decelerationRate={0.2}
+                renderItem={({ item, index }) => {
+                  const inputRange = [
+                    -1,
+                    0,
+                    ITEM_SIZE * index,
+                    ITEM_SIZE * (index + 2),
+                  ];
+                  const opacityRange = [
+                    -1,
+                    0,
+                    ITEM_SIZE * index,
+                    ITEM_SIZE * (index + 0.5),
+                  ];
+
+                  const scale = scrollY.interpolate({
+                    inputRange,
+                    outputRange: [1, 1, 1, 0],
+                  });
+                  const opacity = scrollY.interpolate({
+                    inputRange: opacityRange,
+                    outputRange: [1, 1, 1, 0],
+                  });
+                  const isChecked = completedTasks.includes(index);
+
+                  return (
+                    <Animated.View
+                      className="w-[320px]"
+                      style={{ transform: [{ scale }], opacity }}
                     >
-                      <View className="flex-row justify-between">
-                        <Text className="text-lg font-medium">
-                          Day {challenge.day}
+                      <View
+                        className="bg-blue-400 rounded-xl gap-3 p-[15px] mt-[8.3px] "
+                        style={{}}
+                      >
+                        <View className="flex-row justify-between ">
+                          <Text className="font-semibold opacity-[0.6px]">
+                            Day {item.day}
+                          </Text>
+                          <Checkbox
+                            value={isChecked}
+                            onValueChange={() => handleCheckboxChange(index)}
+                            color={isChecked ? "#2563eb" : "#334155"}
+                          />
+                        </View>
+                        <Text className="text-[16px] font-semibold">
+                          Approach the net 10 times
                         </Text>
-                        <Checkbox
-                          value={completedTasks.includes(index)}
-                          onValueChange={() => {
-                            if (completedTasks.includes(index)) {
-                              setCompletedTasks(
-                                completedTasks.filter((task) => task !== index)
-                              ); // Uncheck logic
-                            } else {
-                              setCompletedTasks((prev) => [...prev, index]); // Check logic
-                              if (index < challenges.length - 1) {
-                                setTimeout(() => {
-                                  scrollViewRef.current?.scrollTo({
-                                    x: (index + 1) * cardWidth, // Move to the next card
-                                    animated: true,
-                                  });
-                                  setCurrentTask(index + 1);
-                                }, 300); // Delay to visually confirm the check
-                              }
-                            }
-                          }}
-                          color={
-                            completedTasks.includes(index) ? "#4ADE80" : "black"
-                          }
-                        />
                       </View>
-                      <Text className="text-[15px] font-semibold">
-                        {challenge.text}
-                      </Text>
-                    </View>
-                  </View>
-                ))}
-              </ScrollView>
-              {/* Carousel indicators */}
-
-              <View
-                className="flex-row gap-1 justify-center items-center mt-3"
-                // style={{ transform: [{ translateY:  }] }}
-              >
-                {challenges.map((_, index) => (
-                  <View
-                    key={index}
-                    style={{
-                      height: 4,
-                      width: index === currentTask ? 14 : 14,
-                      backgroundColor:
-                        index === currentTask ? "white" : "#60A5FA",
-                      borderRadius: 2,
-                      marginHorizontal: 3,
-                    }}
-                  />
-                ))}
-              </View>
-            </View>
-          </View>
-
-          <View className="Header mt-5 flex-row justify-between mb-5">
-            <Text className="text-textColor font-semibold text-[25px]">
-              Train
-            </Text>
-            <TouchableOpacity onPress={() => router.navigate("/training")}>
-              <Text className="text-green-400 font-[500] text-[15px] p-2">
-                See All
-              </Text>
-            </TouchableOpacity>
-          </View>
-
-          <View className="flex-wrap flex-row gap-4">
-            <View className="bg-cyan-400 rounded-md h-[90px]  flex-col gap-4 pl-3  pt-[12px]   min-w-[30%]">
-              <FontAwesome5
-                name="running"
-                size={30}
-                color="rgba(0, 0, 0, 0.5)"
-                style={{ opacity: 0.6 }}
+                    </Animated.View>
+                  );
+                }}
               />
-              <Text className="text-white font-bold text-[16px]">
-                Groundies
-              </Text>
             </View>
-
-            <View className="bg-teal-400 rounded-md h-[90px]  flex-col gap-4 pl-3 pt-[12px]  min-w-[30%]">
-              <FontAwesome5
-                name="running"
-                size={30}
-                color="rgba(0, 0, 0, 0.5)"
-                style={{ opacity: 0.6 }}
-              />
-              <Text className="text-white font-bold text-[16px]">Net Game</Text>
-            </View>
-
-            <View className="bg-pink-400 rounded-md h-[90px]  flex-col gap-4 pl-3 pt-[12px] pr-4 min-w-[30%]">
-              <FontAwesome5
-                name="running"
-                size={30}
-                color="rgba(0, 0, 0, 0.5)"
-                style={{ opacity: 0.6 }}
-              />
-              <Text className="text-white font-bold text-[16px]">Fitness</Text>
-            </View>
-          </View>
-
-          <View className="Header mt-5 flex-row justify-between mb-4 ">
-            <Text className="text-textColor font-semibold text-[25px]">
-              Train
-            </Text>
-            <TouchableOpacity>
-              <Text className="text-green-400 font-[500] text-[15px] p-2">
-                See All
-              </Text>
-            </TouchableOpacity>
-          </View>
-          <View className="gap-2">
-            <TouchableOpacity>
-              <View className="box-view   bg-gray-800  rounded-xl    border-[0.4px] p-3 ">
-                <View className="bg-blue-400 rounded-md gap-3 p-3 ">
-                  <View className="flex-row justify-between">
-                    <Text className="text-lg font-semibold">
-                      Improve Baseline
-                    </Text>
-                  </View>
-                  <Text>Training focusing on being solid on the baseline</Text>
-                </View>
-              </View>
-            </TouchableOpacity>
-
-            <TouchableOpacity>
-              <View className="box-view   bg-gray-800  rounded-xl    border-[0.4px] p-3">
-                <View className="bg-blue-500 rounded-md gap-3 p-3 ">
-                  <View className="flex-row justify-between">
-                    <Text className="text-lg font-semibold">
-                      Improve Net Game
-                    </Text>
-                  </View>
-                  <Text>Training focusing on the upper part of the court</Text>
-                </View>
-              </View>
-            </TouchableOpacity>
           </View>
         </View>
-      </ScrollView>
+
+        <View className="Header mt-5 flex-row justify-between mb-5">
+          <Text className="text-textColor font-semibold text-[25px]">
+            Train
+          </Text>
+          <TouchableOpacity onPress={() => router.push("/training")}>
+            <Text className="text-green-400 font-[500] text-[15px] p-2">
+              See All
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+        <View className="flex-wrap flex-row gap-4">
+          <View className="bg-cyan-400 rounded-md h-[90px]  flex-col gap-4 pl-3  pt-[12px]   min-w-[30%]">
+            <FontAwesome5
+              name="running"
+              size={30}
+              color="rgba(0, 0, 0, 0.5)"
+              style={{ opacity: 0.6 }}
+            />
+            <Text className="text-white font-bold text-[16px]">Groundies</Text>
+          </View>
+
+          <View className="bg-teal-400 rounded-md h-[90px]  flex-col gap-4 pl-3 pt-[12px]  min-w-[30%]">
+            <FontAwesome5
+              name="running"
+              size={30}
+              color="rgba(0, 0, 0, 0.5)"
+              style={{ opacity: 0.6 }}
+            />
+            <Text className="text-white font-bold text-[16px]">Net Game</Text>
+          </View>
+
+          <View className="bg-pink-400 rounded-md h-[90px]  flex-col gap-4 pl-3 pt-[12px] pr-4 min-w-[30%]">
+            <FontAwesome5
+              name="running"
+              size={30}
+              color="rgba(0, 0, 0, 0.5)"
+              style={{ opacity: 0.6 }}
+            />
+            <Text className="text-white font-bold text-[16px]">Fitness</Text>
+          </View>
+        </View>
+
+        <View className="Header mt-5 flex-row justify-between mb-4 ">
+          <Text className="text-textColor font-semibold text-[25px]">
+            Train
+          </Text>
+          <TouchableOpacity>
+            <Text className="text-green-400 font-[500] text-[15px] p-2">
+              See All
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <View className="gap-2">
+          <TouchableOpacity>
+            <View className="box-view   bg-gray-800  rounded-xl    border-[0.4px] p-3 ">
+              <View className="bg-blue-400 rounded-md gap-3 p-3 ">
+                <View className="flex-row justify-between">
+                  <Text className="text-lg font-semibold">
+                    Improve Baseline
+                  </Text>
+                </View>
+                <Text>Training focusing on being solid on the baseline</Text>
+              </View>
+            </View>
+          </TouchableOpacity>
+
+          <TouchableOpacity>
+            <View className="box-view   bg-gray-800  rounded-xl    border-[0.4px] p-3">
+              <View className="bg-blue-500 rounded-md gap-3 p-3 ">
+                <View className="flex-row justify-between">
+                  <Text className="text-lg font-semibold">
+                    Improve Net Game
+                  </Text>
+                </View>
+                <Text>Training focusing on the upper part of the court</Text>
+              </View>
+            </View>
+          </TouchableOpacity>
+        </View>
+        {/*
+        <View className="bg-blue-500 rounded-md gap-3 p-3 my-5">
+          <View className="flex-row justify-between">
+            <Text className="text-lg font-semibold">Forehand and Backhand</Text>
+            <Text>Check</Text>
+          </View>
+          <Text>Make less than 10 net errors</Text>
+        </View> */}
+      </View>
     </SafeAreaView>
   );
 }
